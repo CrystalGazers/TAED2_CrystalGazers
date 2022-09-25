0f2c96629bd7374c2663c80e83e5bf31f489276d
Miquel Sirera <miquelsirera@macsirera.local>
2022-09-25 11:25:06 +0200

    Get connection parametres from .json

--- models/test_connection.py
+++ models/test_connection.py
@@ -1,8 +1,13 @@
 import boto3
 import io
 import pandas as pd
+import json
 
-# boto3.setup_default_session(profile_name='cgmiquel')
+# Setting hyperparameters
+with open('config.json') as config:
+    hyperparams = json.load(config)
+
+boto3.setup_default_session(profile_name=hyperparams['AWS_PROFILE'])
 
 # def download_files(bucket_name, download_path, save_as=None):
 #     s3_client = boto3.client('s3')
@@ -14,5 +19,5 @@ def download_s3(bucket_name, path_to_file):
     return obj
 
 #download_files("arn:aws:s3:::crystal-gazers", "s3://crystal-gazers/archive/ca-all/ca.wiki.test.tokens", "test.tokens")
-df = download_s3("crystal-gazers", "archive/ca-all/ca.wiki.test.tokens")
+df = download_s3(hyperparams['BUCKET_NAME'], hyperparams['TEST_ROOT'])
 print(df)
