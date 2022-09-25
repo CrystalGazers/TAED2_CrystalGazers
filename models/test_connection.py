f4854ff711a9f9ff251b25537b80318a274c0e16
jguaschmarti <jguaschmarti@gmail.com>
2022-09-25 11:00:24 +0200

    Modify s3 download function

--- models/test_connection.py
+++ models/test_connection.py
@@ -1,9 +1,18 @@
 import boto3
+import io
+import pandas as pd
 
-boto3.setup_default_session(profile_name='cgmiquel')
+# boto3.setup_default_session(profile_name='cgmiquel')
 
-def download_files(bucket_name, download_path, save_as=None):
+# def download_files(bucket_name, download_path, save_as=None):
+#     s3_client = boto3.client('s3')
+#     s3_client.download_file(bucket_name, download_path, save_as)
+
+def download_s3(bucket_name, path_to_file):
     s3_client = boto3.client('s3')
-    s3_client.download_file(bucket_name, download_path, save_as)
+    obj = s3_client.get_object(Bucket=bucket_name, Key=path_to_file)
+    return obj
 
-download_files("arn:aws:s3:::crystal-gazers", "s3://crystal-gazers/archive/ca-all/ca.wiki.test.tokens", "test.tokens")
+#download_files("arn:aws:s3:::crystal-gazers", "s3://crystal-gazers/archive/ca-all/ca.wiki.test.tokens", "test.tokens")
+df = download_s3("crystal-gazers", "archive/ca-all/ca.wiki.test.tokens")
+print(df)
