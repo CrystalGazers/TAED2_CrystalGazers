ae65c96c5638c15f60a96946cb37cd38f064fd44
Miquel Sirera <miquelsirera@10-192-44-96client.eduroam.upc.edu>
2022-10-04 14:57:47 +0200

    Change in prediction.py

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,11 +1,9 @@
+import torch
+from transformer import Predictor
+
 # Load model somehow
+model = Predictor()
+model.load_state_dict(torch.load(PATH))
 
-# 'El Periodico' test dataset
-valid_x_df = pd.read_csv(f'{COMPETITION_ROOT}/x_test.csv')
-test_x = valid_x_df[tokens].apply(vocab.get_index).to_numpy(dtype='int32')
-y_pred = validate(model, None, test_x, None, params.batch_size, device)
-y_token = [vocab.idx2token[index] for index in y_pred]
+# Import test dataset from amazon
 
-submission = pd.DataFrame({'id':valid_x_df['id'], 'token': y_token}, columns=['id', 'token'])
-print(submission.head())
-submission.to_csv('submission.csv', index=False)
