54360e909436442775a0ec061deb56944e34674d
Guasch Marti <jguasch@bcd0743377e8.ant.amazon.com>
2022-10-11 15:47:09 +0200

    Deploy notebook and train.py for SageMaker

---
+++ src/models/SM_train_model.py
@@ -0,0 +1,197 @@
+import argparse
+import os
+import pickle
+import numpy as np
+import torch
+from torch import nn
+from transformer import Predictor
+
+if __name__ =='__main__':
+
+    parser = argparse.ArgumentParser()
+
+    # hyperparameters sent by the client are passed as command-line arguments to the script.
+    parser.add_argument('--epochs', type=int, default=5)
+    parser.add_argument('--batch-size', type=int, default=64)
+    parser.add_argument('--use-cuda', type=bool, default=False)
+    parser.add_argument('--embedding_dim', type=int, default=256)
+    parser.add_argument('--num_heads', type=int, default=1)
+
+    # Data, model, and output directories
+    parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
+    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--train', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
+    # parser.add_argument('--test', type=str, default=os.environ['SM_CHANNEL_TEST'])
+
+    args, _ = parser.parse_known_args()
+
+class Vocabulary(object):
+    def __init__(self, pad_token='<pad>', unk_token='<unk>', eos_token='<eos>'):
+        self.token2idx = {}
+        self.idx2token = []
+        self.pad_token = pad_token
+        self.unk_token = unk_token
+        self.eos_token = eos_token
+        if pad_token is not None:
+            self.pad_index = self.add_token(pad_token)
+        if unk_token is not None:
+            self.unk_index = self.add_token(unk_token)
+        if eos_token is not None:
+            self.eos_index = self.add_token(eos_token)
+
+    def add_token(self, token):
+        if token not in self.token2idx:
+            self.idx2token.append(token)
+            self.token2idx[token] = len(self.idx2token) - 1
+        return self.token2idx[token]
+
+    def get_index(self, token):
+        if isinstance(token, str):
+            return self.token2idx.get(token, self.unk_index)
+        else:
+            return [self.token2idx.get(t, self.unk_index) for t in token]
+
+    def __len__(self):
+        return len(self.idx2token)
+
+    def save(self, filename):
+        with open(filename, 'wb') as f:
+            pickle.dump(self.__dict__, f)
+
+    def load(self, filename):
+        with open(filename, 'rb') as f:
+            self.__dict__.update(pickle.load(f))
+        # f = download_s3(args.SM_CHANNEL_TRAIN, filename)
+        # self.__dict__.update(pickle.load(f))
+
+
+def batch_generator(idata, target, batch_size, shuffle=True):
+    nsamples = len(idata)
+    if shuffle:
+        perm = np.random.permutation(nsamples)
+    else:
+        perm = range(nsamples)
+
+    for i in range(0, nsamples, batch_size):
+        batch_idx = perm[i:i+batch_size]
+        if target is not None:
+            yield idata[batch_idx], target[batch_idx]
+        else:
+            yield idata[batch_idx], None
+
+
+def load_preprocessed_dataset(prefix):
+    # Try loading precomputed vocabulary and preprocessed data files
+    token_vocab = Vocabulary()
+    token_vocab.load(f'{prefix}.vocab')
+    data = []
+    for part in ['train', 'valid']:
+         with np.load(f'{prefix}.{part}.npz') as set_data:
+            idata, target = set_data['idata'], set_data['target']
+            data.append((idata, target))
+            print(f'Number of samples ({part}): {len(target)}')
+    print("Using precomputed vocabulary and data files")
+    print(f'Vocabulary size: {len(token_vocab)}')
+    return token_vocab, data
+
+
+def train(model, criterion, optimizer, idata, target, batch_size, device, log=False):
+    model.train()
+    total_loss = 0
+    ncorrect = 0
+    ntokens = 0
+    niterations = 0
+    for X, y in batch_generator(idata, target, batch_size, shuffle=True):
+        # Get input and target sequences from batch
+        X = torch.tensor(X, dtype=torch.long, device=device)
+        y = torch.tensor(y, dtype=torch.long, device=device)
+
+        model.zero_grad()
+        output = model(X)
+        loss = criterion(output, y)
+        loss.backward()
+        optimizer.step()
+        # Training statistics
+        total_loss += loss.item()
+        ncorrect += (torch.max(output, 1)[1] == y).sum().item()
+        ntokens += y.numel()
+        niterations += 1
+        if niterations == 200 or niterations == 500 or niterations % 1000 == 0:
+            print(f'Train: wpb={ntokens//niterations}, num_updates={niterations}, accuracy={100*ncorrect/ntokens:.1f}, loss={total_loss/ntokens:.2f}')
+
+    total_loss = total_loss / ntokens
+    accuracy = 100 * ncorrect / ntokens
+    if log:
+        print(f'Train: wpb={ntokens//niterations}, num_updates={niterations}, accuracy={accuracy:.1f}, loss={total_loss:.2f}')
+    return accuracy, total_loss
+
+def validate(model, criterion, idata, target, batch_size, device):
+    model.eval()
+    total_loss = 0
+    ncorrect = 0
+    ntokens = 0
+    niterations = 0
+    y_pred = []
+    with torch.no_grad():
+        for X, y in batch_generator(idata, target, batch_size, shuffle=False):
+            # Get input and target sequences from batch
+            X = torch.tensor(X, dtype=torch.long, device=device)
+            output = model(X)
+            if target is not None:
+                y = torch.tensor(y, dtype=torch.long, device=device)
+                loss = criterion(output, y)
+                total_loss += loss.item()
+                ncorrect += (torch.max(output, 1)[1] == y).sum().item()
+                ntokens += y.numel()
+                niterations += 1
+            else:
+                pred = torch.max(output, 1)[1].detach().to('cpu').numpy()
+                y_pred.append(pred)
+
+    if target is not None:
+        total_loss = total_loss / ntokens
+        accuracy = 100 * ncorrect / ntokens
+        return accuracy, total_loss
+    else:
+        return np.concatenate(y_pred)
+
+# Create working dir (check this!!)
+# pathlib.Path(WORKING_ROOT).mkdir(parents=True, exist_ok=True)
+
+# Select device
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+elif torch.backends.mps.is_available():
+    device = torch.device('mps')
+    print("Training on MPS")
+else:
+    device = torch.device('cpu')
+    print("WARNING: Training without GPU can be very slow!")
+
+# Change this according to config
+vocab, data = load_preprocessed_dataset(args.SM_CHANNEL_TRAIN)
+
+# Load model
+model = Predictor(len(vocab), args.embedding_dim, num_heads=args.num_heads).to(device)
+
+print(model)
+for name, param in model.named_parameters():
+    print(f'{name:20} {param.numel()} {list(param.shape)}')
+print(f'TOTAL                {sum(p.numel() for p in model.parameters())}')
+
+optimizer = torch.optim.Adam(model.parameters())
+criterion = nn.CrossEntropyLoss(reduction='sum')
+
+optimizer = torch.optim.Adam(model.parameters())
+criterion = nn.CrossEntropyLoss(reduction='sum')
+
+train_accuracy = []
+wiki_accuracy = []
+valid_accuracy = []
+for epoch in range(args.epochs):
+    acc, loss = train(model, criterion, optimizer, data[0][0], data[0][1], args.batch_size, device, log=True)
+    train_accuracy.append(acc)
+    print(f'| epoch {epoch:03d} | train accuracy={acc:.1f}%, train loss={loss:.2f}')
+    acc, loss = validate(model, criterion, data[1][0], data[1][1], args.batch_size, device)
+    wiki_accuracy.append(acc)
+    print(f'| epoch {epoch:03d} | valid accuracy={acc:.1f}%, valid loss={loss:.2f} (wikipedia)')
