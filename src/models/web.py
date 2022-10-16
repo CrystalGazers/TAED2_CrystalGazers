d31e45929605ae51d603a7062c7dc0c8d595575c
Miquel Sirera <miquelsirera@macsirera.local>
2022-10-16 15:54:07 +0200

    Pylint corrections on web.py

--- src/models/web.py
+++ src/models/web.py
@@ -1,29 +1,28 @@
-import streamlit as st
 import json
+import streamlit as st
 import matplotlib
 import boto3
 
 #CONFIG
-boto3.setup_default_session(profile_name='webapi')
+boto3.setup_default_session(profile_name='cgmiquel')
 client = boto3.client('sagemaker-runtime')
-#URL = 'https://runtime.sagemaker.eu-west-3.amazonaws.com/endpoints/pytorch-inference-2022-10-15-19-35-33-706/invocations'
-
 
 def fetch(text):
+    """Function to post a request to SageMaker endpoint. The input needs to follow the next format:
+       For the sentence Prev1 Prev2 Prev3 Word Next1 Next2 Next3, where the model needs to predict
+       the Word word, text needs to be ["Prev1", "Prev2", "Prev3", "Next1", "Next2", "Next3"].
+       The output of the request, will be the predicted central word, Word."""
     try:
         #Sagemaker request
         data = {'input': text}
         body = str.encode(json.dumps(data))
         response = client.invoke_endpoint(
-            EndpointName='pytorch-inference-2022-10-16-11-01-23-464', 
+            EndpointName='pytorch-inference-2022-10-16-11-01-23-464',
             ContentType='application/json',
             Accept='application/json',
-            Body=body
-            )
+            Body=body)
         res_json = json.loads(response['Body'].read().decode("utf-8"))
-        #print(json.load(response))
         return res_json, 1
-        
     except Exception as err:
         return err, 0
 
@@ -32,7 +31,6 @@ try:
 
     font = {'weight': 'normal',
         'size': 22}
-    
     matplotlib.rc('font', **font)
 
     st.title('Crystal gazers')
@@ -44,20 +42,18 @@ try:
         submitted = st.form_submit_button("Submit")
 
         if submitted:
-            input = text.split()
-            if len(input) == 6:
+            text = text.split()
+            if len(text) == 6:
                 st.write("Results")
-                res, status = fetch(input)
+                res, status = fetch(text)
                 if status:
                     st.write(res)
                 else:
                     st.error("Error")
             else:
-                st.error("Input size needs to be 6 words, 3 on the left of the desired word and 3 on the right. Please try again.")
-    
-
+                st.error("Input size needs to be 6 words, 3 on the left \
+                    of the desired word and 3 on the right. Please try again.")
 except NotImplementedError as nie:
     print(f'[ERROR] {nie}')
-    pass
 except Exception as e:
     raise e
