03861f149bbd5ea6a9220650f8431573a49063cb
mashazya <maria.zyatyugina@estudiantat.upc.edu>
2022-10-16 12:26:25 +0200

    Pytest API and minor changes

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,8 +1,9 @@
-from inference import input_fn, model_fn, output_fn, predict_fn
+from inference import fetch, model_fn
+
 
 class TestClass:
     '''The class contains multiple functions to test
-    the defined by Crystal Gazers predictor model.'''
+    the defined by Crystal Gazers API.'''
 
     model = model_fn('.')
     accepted_output_test1 = ['va', 'camina', 'esta', 'existeix', 'conegut']
@@ -13,25 +14,18 @@ class TestClass:
         '''The function checks whether the given input is
         contained in the corresponding accepted output'''
 
-        input_one = input_fn('{"input": ["un", "monjo", "vell", "per", "la", "muntanya"]}',
-                             'application/json')
-        pred_one = predict_fn(input_one, self.model)
-        out_one = output_fn(pred_one, 'application/json')[1:-1]
+        out_one = fetch("un monjo vell per la muntanya")
         assert out_one in self.accepted_output_test1
 
     def test_two(self):
         '''The function checks whether the given input is
         contained in the corresponding accepted output'''
-        input_two = input_fn('{"input": ["la", "noia", "rossa", "a", "la", "biblioteca"]}',
-                             'application/json')
-        pred_two = predict_fn(input_two, self.model)
-        out_two = output_fn(pred_two, 'application/json')[1:-1]
+
+        out_two = fetch("la noia rossa a la biblioteca")
         assert out_two in self.accepted_output_test2
     def test_three(self):
         '''The function checks whether the given input is
         contained in the corresponding accepted output'''
-        input_three = input_fn('{"input": ["el", "video", "de", "setmana", "passada", "es"]}',
-                              'application/json')
-        pred_three = predict_fn(input_three, self.model)
-        out_three = output_fn(pred_three, 'application/json')[1:-1]
+
+        out_three = fetch("el video de setmana passada es")
         assert out_three in self.accepted_output_test3
